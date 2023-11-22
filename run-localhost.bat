ea7492e5c58d567c0851e983ec655dd35847332e
Yeray Medina López <5522922+ymedlop@users.noreply.github.com>
2023-11-22 23:36:49 +0100

    update

--- run-localhost.bat
+++ run-localhost.bat
@@ -1,5 +1,5 @@
 kubectl kustomize clusters\groups\localhost\docker-desktop --enable-helm --load-restrictor LoadRestrictionsNone | kubectl apply -f -
 
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}"
 
 kubectl port-forward svc/argocd-server -n argocd 8080:443
