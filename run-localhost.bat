657fbf1064920fb36ad426511765287e9ebb8e2e
Yeray Medina López <5522922+ymedlop@users.noreply.github.com>
2024-01-13 18:40:57 +0100

    update

--- run-localhost.bat
+++ run-localhost.bat
@@ -2,6 +2,6 @@ ECHO TODO: https://github.com/kubernetes-sigs/kustomize/issues/3502
 kubectl kustomize clusters\groups\on-prem\dev\docker-desktop --enable-helm --load-restrictor LoadRestrictionsNone | kubectl apply -f -
 kubectl kustomize clusters\groups\on-prem\dev\docker-desktop --enable-helm --load-restrictor LoadRestrictionsNone | kubectl apply -f -
 
-kubectl -n argo get secret argocd-initial-admin-secret -o jsonpath="{.data.password}"
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}"
 
-kubectl port-forward svc/argocd-server -n argo 8080:80
+kubectl port-forward svc/argocd-server -n argocd 8080:80
