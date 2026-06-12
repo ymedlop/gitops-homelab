db01334b6e01fb788007d3880d5685a77a4aa626
Yeray Medina Lopez <5522922+ymedlop@users.noreply.github.com>
2026-06-12 22:53:08 +0200

    Delete run-localhost.bat

--- run-localhost.bat
+++
@@ -1,5 +0,0 @@
-kubectl kustomize clusters\groups\on-prem\in-cluster --enable-helm --load-restrictor LoadRestrictionsNone | kubectl apply -f -
-
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}"
-
-ECHO kubectl port-forward svc/argocd-server -n argocd 8080:80
